a0187c2125fe3763a6aaa9d5cdf2ce641557eb2f
magrega <proshanov@yandex.ru>
2025-06-01 16:07:04 +0300

    Add project files.

---
+++ BigPictureManager/Form1.cs
@@ -0,0 +1,32 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BigPictureManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IEnumerable<CoreAudioDevice> devices = new CoreAudioController().GetPlaybackDevices();
+
+
+            foreach (CoreAudioDevice d in devices)
+            {
+                if (!d.IsDefaultDevice)
+                {
+                    Console.WriteLine(d.FullName);
+                    d.SetAsDefault();
+                    return;
+                }
+            }
+
+        }
+    }
+}
