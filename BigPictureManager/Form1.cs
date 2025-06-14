650eaf53198b3892abe5ff3d77feae202c292870
magrega <proshanov@yandex.ru>
2025-06-14 23:39:35 +0300

    feat: Add bluetooth turn off

--- BigPictureManager/Form1.cs
+++ BigPictureManager/Form1.cs
@@ -1,30 +1,62 @@
-﻿using AudioSwitcher.AudioApi.CoreAudio;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Enumeration;
-
-public class XboxController
-{
-    [DllImport("XInput1_4.dll", EntryPoint = "#103")]
-    private static extern int FnOff(int controllerIndex);
-
-    public static void TurnOff(int controllerIndex = 0)
-    {
-        FnOff(controllerIndex); // Tries to turn off controller 0-3
-    }
-}
+using Windows.Devices.Radios;
 
 
 
 namespace BigPictureManager
 {
+
+
     public partial class Form1 : Form
     {
+
+        public async Task<bool> TurnOffBluetoothAsync()
+        {
+            try
+            {
+                // Request access to radios
+                var access = await Radio.RequestAccessAsync();
+                if (access != RadioAccessStatus.Allowed)
+                {
+                    Console.WriteLine("Permission denied to control radios");
+                    return false;
+                }
+
+                // Get all radios
+                var radios = await Radio.GetRadiosAsync();
+
+                // Find the Bluetooth radio
+                var bluetoothRadio = radios.FirstOrDefault(radio => radio.Kind == RadioKind.Bluetooth);
+
+                if (bluetoothRadio == null)
+                {
+                    Console.WriteLine("Bluetooth radio not found");
+                    return false;
+                }
+
+                // Turn off Bluetooth
+                if (bluetoothRadio.State != RadioState.Off)
+                {
+                    await bluetoothRadio.SetStateAsync(RadioState.Off);
+                    Console.WriteLine("Bluetooth turned off successfully");
+                    return true;
+                }
+
+                Console.WriteLine("Bluetooth is already off");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -32,7 +64,9 @@ namespace BigPictureManager
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            XboxController.TurnOff();
+
+
+
             bool IsProcessRunning(string processName)
             {
                 Process[] processes = Process.GetProcessesByName(processName);
@@ -40,14 +74,14 @@ namespace BigPictureManager
             }
 
             // Usage:
-            if (IsProcessRunning("notepad"))
-            {
-                Console.WriteLine("Notepad is running!");
-            }
-            else
-            {
-                Console.WriteLine("Notepad is NOT running.");
-            }
+            //if (IsProcessRunning("notepad"))
+            //{
+            //    Console.WriteLine("Notepad is running!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Notepad is NOT running.");
+            //}
 
 
 
@@ -58,7 +92,11 @@ namespace BigPictureManager
                 string deviceSelector = BluetoothDevice.GetDeviceSelector();
                 DeviceInformationCollection bTdevices = await DeviceInformation.FindAllAsync(deviceSelector);
 
-                foreach (DeviceInformation d in bTdevices)
+                DeviceInformationCollection PairedBluetoothDevices =
+       await DeviceInformation.FindAllAsync(BluetoothDevice.GetDeviceSelectorFromPairingState(true));
+
+
+                foreach (DeviceInformation d in PairedBluetoothDevices)
                 {
                     Console.WriteLine(d.Name);
                 }
@@ -69,20 +107,19 @@ namespace BigPictureManager
                 //Console.WriteLine($"Disconnected: {targetDevice.Name}");
             }
 
+            await TurnOffBluetoothAsync();
 
-            await DisconnectBluetoothDeviceAsync();
+            //IEnumerable<CoreAudioDevice> devices = new CoreAudioController().GetPlaybackDevices();
 
-            IEnumerable<CoreAudioDevice> devices = new CoreAudioController().GetPlaybackDevices();
-
-            foreach (CoreAudioDevice d in devices)
-            {
-                if (!d.IsDefaultDevice)
-                {
-                    Console.WriteLine(d.FullName);
-                    d.SetAsDefault();
-                    return;
-                }
-            }
+            //foreach (CoreAudioDevice d in devices)
+            //{
+            //    if (!d.IsDefaultDevice)
+            //    {
+            //        Console.WriteLine(d.FullName);
+            //        d.SetAsDefault();
+            //        return;
+            //    }
+            //}
 
         }
     }
